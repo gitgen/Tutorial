1fbcdc7db1a08000f37acad613b1f4433302e623
PJs <preeya.joshi12@imperial.ac.uk>
2014-02-23 02:28:28 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# Expected Output: 
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10 
+
+def count_to_ten():
+    for i in range(10):
+        print i
+
+if __name__ == '__main__':
+    count_to_ten()
